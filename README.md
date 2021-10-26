5832e2eab75035e05fbbc109d8370fb3dd4d0636
Andrei Neagoie <aneagoie@MacBook-Pro-2.lan>
2021-10-26 11:11:47 -0400

    updates

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Final project for ZTM course
 4. You must add your own API key in the `src/App.js` file to connect to Clarifai.
 
 You can grab Clarifai API key [here](https://www.clarifai.com/)
+
--- src/App.js
+++ src/App.js
@@ -81,15 +81,15 @@ class App extends Component {
     this.setState({imageUrl: this.state.input});
     app.models
       .predict(
-        // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-        // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-        // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
-        // so you would change from:
-        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-        // to:
-        // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
+    // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+    // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+    // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+    // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+    // is to use a different version of their model that works like the ones found here: https://github.com/Clarifai/clarifai-javascript/blob/master/src/index.js
+    // so you would change from:
+    // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    // to:
+    // .predict('53e1df302c079b3db8a0a36033ed2d15', this.state.input)
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
       .then(response => {
