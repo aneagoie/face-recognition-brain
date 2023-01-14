44b6c358160d8657cdd8d6498fbecf44b0f4127b
r4pt0s <r4pt0s@protonmail.com>
2023-01-14 17:25:50 +0100

    Add rest of existing code

--- src/App.js
+++ src/App.js
@@ -92,6 +92,24 @@ class App extends Component {
             },
             this.state.input
         )
+        .then((response) => {
+            console.log('hi', response);
+            if (response) {
+                fetch('http://localhost:3000/image', {
+                    method: 'put',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        id: this.state.user.id,
+                    }),
+                })
+                    .then((response) => response.json())
+                    .then((count) => {
+                        this.setState(Object.assign(this.state.user, { entries: count }));
+                    });
+            }
+            this.displayFaceBox(this.calculateFaceLocation(response));
+        })
+        .catch((err) => console.log(err));
   }
 
   onRouteChange = (route) => {
