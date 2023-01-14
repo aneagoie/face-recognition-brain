540b3c9e1a7cf57872ae9cf96c94f96796dc742b
Andrei Neagoie <aneagoie@Andreis-MacBook-Pro-2.local>
2023-01-14 16:51:29 -0300

    update

--- src/App.js
+++ src/App.js
@@ -81,19 +81,24 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models
-      .predict(
+   
     // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
     // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
     // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-    // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-    // is to use a different version of their model that works like the ones found here: https://github.com/Clarifai/clarifai-javascript/blob/master/src/index.js
-    // so you would change from:
-    // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-    // to:
-    // .predict('53e1df302c079b3db8a0a36033ed2d15', this.state.input)
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
+    // If that isn't working, then that means you will have to wait until their servers are back up. 
+
+    // Old Way:
+    // app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+
+    // New Way:
+    app.models
+      .predict(
+        {
+          id: 'face-detection',
+          name: 'face-detection',
+          version: '6dc7e46bc9124c5c8824be4822abe105',
+          type: 'visual-detector',
+        }, this.state.input)
       .then(response => {
         console.log('hi', response)
         if (response) {
